92f735375162a21bb9ebbdd80f8542f169cbd9c4
imPBH <alex.provo31@gmail.com>
2022-10-28 17:47:01 +0200

    Init plugin.php

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Admin Notifications
+ * Plugin URI: https://www.your-site.com/
+ * Description: Send information to a webhook when a new comment or a new post is posted.
+ * Version: 0.1
+ * Author: Alexis Provo
+ * Author URI: https://github.com/imPBH
+ **/
+?>
