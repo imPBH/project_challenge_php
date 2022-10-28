8f7e1d5f514367992a20439f2a9d5cced32467a6
imPBH <alex.provo31@gmail.com>
2022-10-28 22:43:39 +0200

    Send message to slack webhook when the status of a comment changes

--- Services/Slack.php
+++ Services/Slack.php
@@ -128,6 +128,148 @@ class Slack implements IService
 
     public function CommentStatusUpdate($comment, $postUrl, $postTitle, $author, $oldStatus, $newStatus, $timestamp)
     {
-        // TODO: Implement CommentStatusUpdate() method.
+        $title = ":tools: Update detected on a comment :tools:";
+        switch ($newStatus) {
+            case "approved":
+                $title = ":white_check_mark: Approved comment :white_check_mark:";
+                break;
+            case "delete":
+                $title = ":x::wastebasket: Comment permanently deleted :wastebasket::x:";
+                break;
+            case "trash":
+                $title = ":wastebasket: Comment trashed :wastebasket:";
+                break;
+            case "spam":
+                $title = ":x: Comment put in the spam section :x:";
+                break;
+            case "unapproved":
+                $title = ":grey_question: Unapproved comment :grey_question:";
+                break;
+        }
+
+        $json_data = json_encode([
+            "blocks" => [
+                [
+                    "type" => "header",
+                    "text" => [
+                        "type" => "plain_text",
+                        "text" => $title
+                    ]
+                ],
+                [
+                    "type" => "divider"
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "*Original post :*"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "<$postUrl|$postTitle>"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "*Author :*"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "$author"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "*Comment :*"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "$comment->comment_content"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "*Old status :* $oldStatus"
+                    ]
+                ],
+                [
+                    "type" => "section",
+                    "text" => [
+                        "type" => "mrkdwn",
+                        "text" => "*New status :* $newStatus"
+                    ]
+                ],
+                [
+                    "type" => "divider"
+                ],
+                [
+                    "type" => "actions",
+                    "elements" => [
+                        [
+                            "type" => "button",
+                            "text" => [
+                                "type" => "plain_text",
+                                "text" => "Approve"
+                            ],
+                            "style" => "primary",
+                            "url" => $this->site . "/wp-admin/comment.php?action=editcomment&c=" . $comment->comment_ID
+                        ],
+                        [
+                            "type" => "button",
+                            "text" => [
+                                "type" => "plain_text",
+                                "text" => "Delete"
+                            ],
+                            "style" => "danger",
+                            "url" => $this->site . "/wp-admin/comment.php?c=" . $comment->comment_ID . "&action=cdc"
+                        ],
+                        [
+                            "type" => "button",
+                            "text" => [
+                                "type" => "plain_text",
+                                "text" => "Spam"
+                            ],
+                            "style" => "danger",
+                            "url" => $this->site . "/wp-admin/comment.php?c=" . $comment->comment_ID . "&action=cdc&dt=spam"
+                        ]
+                    ]
+
+                ],
+                [
+                    "type" => "divider"
+                ],
+                [
+                    "type" => "context",
+                    "elements" => [
+                        [
+                            "type" => "image",
+                            "image_url" => "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png",
+                            "alt_text" => "Wordpress logo"
+                        ],
+                        [
+                            "type" => "plain_text",
+                            "text" => "Wordpress Plugin • " . date("F j, Y, G:i T", $timestamp)
+                        ]
+                    ]
+                ]
+            ]
+        ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        Sender::Send($json_data, $this->webhookUrl);
     }
 }
